4084e1cad12038acf13b3b48c590d78d9bfb7cb4
sanghee <nsh8286@gmail.com>
2020-09-01 20:00:04 +0900

    qnetFL complete!

--- lab06-1_qnetFL_final.py
+++ lab06-1_qnetFL_final.py
@@ -25,11 +25,12 @@ print(type(Wbf))
 Qpred = tf.matmul(inputs, W)
 modelQpred = tf.keras.Model(inputs = inputs, outputs = Qpred)
 '''
-class MyModel():
+class MyModel(tf.keras.Model):
     def __init__(self):
+        super().__init__(name = 'my_model')
         self.W = tf.Variable(tf.random.uniform(
                                 [input_size, output_size], 0, 1))#weight
-    def __call__(self,x):
+    def call(self,x):
         return tf.matmul(x,self.W)
 
 modelQpred = MyModel()
@@ -37,27 +38,31 @@ Wbf = modelQpred.W.numpy() # W before fit 저장
 
 def loss(predicted_y, desired_y):
     return tf.reduce_sum(tf.square(predicted_y-  desired_y))
+loss_fn = lambda: loss(modelQpred(input), output)
+var_list_fn = lambda: modelQpred.trainable_weights
+
 '''
 modelQpred.compile(loss = loss, optimizer = tf.keras.optimizers.SGD(lr=learning_rate))
 modelQpred.summary()
 '''
+'''
 def train(model, inputs, outputs, learning_rate):
     with tf.GradientTape() as t:
-        current_loss = loss(model(inputs), outputs)
-    dW = t.gradient(current_loss, [model.W])
+        current_loss = loss(model.predict(inputs), outputs)
+    dW = t.gradient(current_loss, model.W)
     print(dW)
     model.W.assign_sub(learning_rate*dW)
+'''
+opt = tf.keras.optimizers.SGD(learning_rate=learning_rate)
 # Set Q-learning related parameters
 dis =.99
-num_episodes = 1
+num_episodes = 2000
 
 #Create lists to contain total rewards and steps per episode
 rList =[]
 
 def one_hot(x):
     return np.identity(16)[x:x+1] # 모양 조심
-
-print(type(one_hot(1)[0][0]))
 for i in range(num_episodes):
     #reset environment and get first new observation
     s = env.reset()
@@ -70,7 +75,7 @@ for i in range(num_episodes):
     while not done:
         # Choose an action by greedily (with e chance of random action)
         # from the Q-network
-        Qs = modelQpred(one_hot(s))
+        Qs = modelQpred.predict(one_hot(s))
         if np.random.rand(1) < e :
             a = env.action_space.sample()
         else:
@@ -84,12 +89,15 @@ for i in range(num_episodes):
         else:
             # Obtain the Q_s1 values by feeding the new state through our
             # network
-            Qs1 = modelQpred(one_hot(s1))
+            Qs1 = modelQpred.predict(one_hot(s1))
             #update Q
             Qs[0,a] = reward + dis*np.max(Qs1)
 
         #train our network using target Y and predicte Q values
-        train(MyModel, one_hot(s), Qs, learning_rate)
+        input = one_hot(s)
+        output = Qs
+        opt.minimize(loss_fn,var_list_fn)
+        '''#for debug
         Waf = modelQpred.W.numpy()
         if (not np.array_equiv(Wbf, Waf)):
             print("W has changed!")
@@ -98,15 +106,14 @@ for i in range(num_episodes):
             print('state : ', s)
             print('one hot : ', one_hot(s))
             print('Qs    : ', Qs)        
-        
+        '''
+        if (done): print(i)
 
 
         rAll += reward
         s = s1
     rList.append(rAll)
 print("Present of succesful episodes: " +
-        str(sum(rList) / num_episodes)+ "%")
+        str(sum(rList) / num_episodes))
 plt.bar(range(len(rList)), rList, color="blue")
 plt.show()
-
-
