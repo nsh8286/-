b7810f9c8edaf7fca7d8c37d3d9bb88416e8e109
sanghee <nsh8286@gmail.com>
2020-09-07 00:10:46 +0900

    add DQN

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import numpy as np
-a = [1,2,3,4]
-b = [5,6,7,8]
-a,b = np.array(a), np.array(b)
-print(b-a)
-print(type(b-a))
+#import tensorflow as tf
+'''
+a = tf.one_hot(np.array([0,1,2,3,2,3]),4)
+print (a)
+predict = tf.constant(np.array([[1,1,1,99],[99,1,1,1],[2,1,99,1],[1,99,1,1],[1,1,1,99],[1,1,1,99]]))
+print (predict)
+print(predict*a.astype('float32'))
+'''
+
+one_hot = np.array([[0,0,0,1],[1,0,0,0],[0,1,0,0],[0,0,0,1],[1,0,0,0],[0,1,0,0]])
+print (one_hot)
+predict = np.array([[1,1,1,99],[99,1,1,1],[2,1,99,1],[1,99,1,1],[1,1,1,99],[1,1,1,99]])
+print (predict)
+print(predict*one_hot)
